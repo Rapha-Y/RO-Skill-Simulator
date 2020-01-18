e20b13acaa6cc1d7286520dd9f144ff9e21a31ef
raphyuki <raph.yuki@gmail.com>
2020-01-17 22:21:22 -0200

    separated skill data from App.js

--- App.js
+++ App.js
@@ -1,52 +1,34 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import skillData from './data/swordsman.js';
 
-export default class FlatListBasics extends Component {
+class FlatListItem extends Component {
   render() {
     return (
-      //FlatList being used inside ScrollView takes away FlatList advantages (renders everything), find solution
-      <ScrollView>
-          <Text style={styles.title}>Swordsman</Text>
-          <FlatList
-            data={[
-              {key: 'Bash'},
-              {key: 'Endure'},
-              {key: 'Increase HP Recovery'},
-              {key: 'Magnum Break'},
-              {key: 'Provoke'},
-              {key: 'Sword Mastery'},
-              {key: 'Two-Handed Sword Mastery'},
-            ]}
-            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-          />
-          <Text style={styles.title}>Knight</Text>
-          <FlatList
-            data={[
-              {key: 'Bowling Bash'},
-              {key: 'Brandish Spear'},
-              {key: 'Cavalier Mastery'},
-              {key: 'Counter Attack'},
-              {key: 'Peco Peco Ride'},
-              {key: 'Pierce'},
-              {key: 'Spear Boomerang'},
-              {key: 'Spear Mastery'},
-              {key: 'Spear Stab'},
-              {key: 'Twohand Quicken'},
-            ]}
-            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-          />
-      </ScrollView>
+      <View style={{padding: 10}}>
+        <Text>{this.props.item.name}</Text>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  title: {
-    backgroundColor: "black",
-    color: "white",
-    padding: 10,
-  },
-  item: {
-    padding: 10,
+export default class SkillList extends Component {
+  render() {
+    return (
+      <View>
+        <FlatList 
+          data={skillData}
+          renderItem={({item, id})=>{
+            return (
+              <FlatListItem
+                item={item} 
+                id={id}
+              />
+            );
+          }}
+        > 
+        </FlatList>
+      </View>
+    );
   }
-})
+}
---
+++ data/novice.js
@@ -0,0 +1,7 @@
+const skillData = [
+    {
+        id: 1,
+        name: "Basic Skill",
+        icon: "../icons/1.png",
+    }
+];
